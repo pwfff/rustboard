d4637d4bce7e55310a21fb69e57bf3d234026c6f
pwf <954340+pwfff@users.noreply.github.com>
2024-08-18 14:06:52 -0500

    lord help me

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use crate::pw_helper::{DEFAULT_CHANNELS, DEFAULT_RATE};
-use ashpd::desktop::global_shortcuts::{GlobalShortcuts, NewShortcut};
-use ashpd::WindowIdentifier;
-use futures::stream::StreamExt;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use pipewire as pw;
 use rodio::source::UniformSourceIterator;
 use rodio::{Decoder, Source};
 use std::fs::File;
 use std::io::BufReader;
-use tauri::{Emitter, Listener, Manager};
 
 mod pw_helper;
 use pw_helper::{pw_thread, PlayBuf};
@@ -17,59 +12,23 @@ use pw_helper::{pw_thread, PlayBuf};
 struct MyState {
     playbuf: PlayBuf,
     pw_sender: pipewire::channel::Sender<PlayBuf>,
-    globals: GlobalShortcuts<'static>,
 }
 
-#[tauri::command]
-async fn greet(state: tauri::State<'_, MyState>) -> Result<(), String> {
-    println!("??");
-    state
-        .pw_sender
-        .send(state.playbuf.clone())
-        .expect("bad send i guess");
-    Ok(())
+#[get("/")]
+async fn hello(data: web::Data<MyState>) -> impl Responder {
+    data.pw_sender.send(data.playbuf.clone()).expect("oops");
+    HttpResponse::Ok().body("Hello world!")
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
     pw::init();
 
     let (pw_sender, pw_receiver) = pipewire::channel::channel();
-
-    let pw_thread = tokio::spawn(async move { pw_thread(pw_receiver) });
-
-    let globals = GlobalShortcuts::new()
-        .await
-        .expect("couldnt make new globalshortcuts");
-    let session = globals
-        .create_session()
-        .await
-        .expect("couldnt create session");
-
-    let desc = "hotkey 1 for rustboard";
-    let shortcut = NewShortcut::new("rustboard-hotkey", desc).preferred_trigger("ALT+SHIFT+a");
-    let mut new_shortcuts = vec![];
-    if !globals
-        .list_shortcuts(&session)
-        .await
-        .expect("couldnt list shortcuts")
-        .response()
-        .expect("bad response")
-        .shortcuts()
-        .iter()
-        .any(|s| s.description() == desc)
-    {
-        new_shortcuts.push(shortcut);
-    };
-    globals
-        .bind_shortcuts(&session, new_shortcuts.as_slice(), &WindowIdentifier::None)
-        .await
-        .expect("couldnt bind shortcuts");
-
-    let mut globals_stream = globals
-        .receive_activated()
-        .await
-        .expect("couldnt start receivin");
+    let pw_thread = actix_web::rt::spawn(async move { pw_thread(pw_receiver) });
 
     // Load a sound from a file, using a path relative to Cargo.toml
     let file = BufReader::new(
@@ -82,44 +41,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .buffered();
     let buf: Vec<i16> = conv.collect();
 
-    let app = tauri::Builder::default()
-        .manage(MyState {
-            pw_sender,
-            globals,
-            playbuf: PlayBuf { buf },
-        })
-        .setup(move |app| {
-            let handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move {
-                loop {
-                    if let Some(_) = globals_stream.next().await {
-                        match handle.emit("hotkey-triggered", "activated") {
-                            Ok(_) => {}
-                            Err(e) => {
-                                println!("error emitting event {:?}", e)
-                            }
-                        };
-                    }
-                }
-            });
-
-            let handle = app.handle().clone();
-            app.listen("hotkey-triggered", move |_| {
-                let state: tauri::State<MyState> = handle.state();
-                state
-                    .pw_sender
-                    .send(state.playbuf.clone())
-                    .expect("bad send i guess");
-            });
-
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!());
-
-    pw_thread.await.expect("cool");
-
-    app.expect("poop");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(MyState {
+                pw_sender: pw_sender.clone(),
+                playbuf: PlayBuf { buf: buf.to_vec() },
+            }))
+            .service(hello)
+    })
+    .shutdown_timeout(3)
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
+    .expect("idk");
 
     Ok(())
 }
