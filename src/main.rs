c484aae253fb5de5e600d90d3fb16c46497e12c0
pwf <954340+pwfff@users.noreply.github.com>
2024-08-24 12:50:28 -0500

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,22 @@
-use crate::pw_helper::{DEFAULT_CHANNELS, DEFAULT_RATE};
 use askama::Template;
 use axum::body::Bytes;
 use axum::extract::{Path, State};
 use axum::response::{Html, IntoResponse, Response};
 use axum::{http::StatusCode, routing::get, routing::post, Router};
-use ebur128::EbuR128;
+use load_audio::process_sound;
 use pipewire as pw;
-use rodio::source::UniformSourceIterator;
-use rodio::{Decoder, Source};
 use serde::{Deserialize, Serialize};
-use slugify::slugify;
 use std::collections::HashMap;
-use std::fs::{DirEntry, File};
-use std::io::BufReader;
 use std::sync::{Arc, RwLock};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod pw_helper;
-use pw_helper::{pw_thread, Message, PlayBuf, TARGET_LUFS};
+use pw_helper::{pw_thread, Message, PlayBuf};
+mod load_audio;
 
 struct MyState {
     playbufs: HashMap<String, PlayBuf>,
     pw_sender: pipewire::channel::Sender<Message>,
-}
-
-fn db_to_amp(db: f32) -> f32 {
-    10f32.powf(db / 20.)
-}
-
-fn amp_to_db(amp: f32) -> f32 {
-    20. * amp.log10()
-}
-
-fn lufs_multiplier(current: f32, target: f32) -> f32 {
-    db_to_amp(target) / db_to_amp(current)
-}
-
-async fn process_sound(path: DirEntry) -> (String, PlayBuf) {
-    let key = slugify!(path.file_name().to_str().unwrap());
-
-    //println!("loading {:?}", key);
-
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open(path.path()).unwrap());
-
-    // Decode that sound file into a source
-    let source = Decoder::new(file).unwrap();
-
-    // convert to known channels and sample rate
-    let conv = UniformSourceIterator::<_, f32>::new(source, DEFAULT_CHANNELS as u16, DEFAULT_RATE)
-        .buffered();
-
-    // measure overall loudness
-    let mut ebur = EbuR128::new(DEFAULT_CHANNELS, DEFAULT_RATE, ebur128::Mode::I).unwrap();
-    let buf: Vec<f32> = conv.clone().collect();
-    ebur.add_frames_f32(&buf).unwrap();
-    //println!(
-    //    "Integrated loudness: {:.1} LUFS",
-    //    ebur.loudness_global().unwrap()
-    //);
-
-    // get float amp value from lufs
-    let amp = lufs_multiplier(ebur.loudness_global().unwrap() as f32, TARGET_LUFS);
-    let adjusted: Vec<f32> = buf.iter().map(|s| *s * amp).collect();
-
-    // measure again
-    //let mut ebur =
-    //    EbuR128::new(DEFAULT_CHANNELS, DEFAULT_RATE, ebur128::Mode::I).unwrap();
-    //ebur.add_frames_f32(&adjusted).unwrap();
-    //println!(
-    //    "Integrated loudness: {:.1} LUFS",
-    //    ebur.loudness_global().unwrap()
-    //);
-
-    (key.clone(), PlayBuf { buf: adjusted })
 }
 
 impl MyState {
@@ -106,9 +49,7 @@ impl MyState {
 
 type SharedState = Arc<RwLock<MyState>>;
 
-async fn stop(
-    State(state): State<SharedState>,
-) -> Result<Bytes, StatusCode> {
+async fn stop(State(state): State<SharedState>) -> Result<Bytes, StatusCode> {
     let state = state.read().unwrap();
     let sender = &state.pw_sender;
     sender.send(Message::Stop()).expect("oops");
